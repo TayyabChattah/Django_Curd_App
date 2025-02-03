9e0abc8d5863045928ba2dd7c54e29ee358c5807
TayyabChattah <tayyyabchattah72@gmail.com>
2025-02-03 14:47:25 +0500

    Read All emp Done

--- emp_app/urls.py
+++ emp_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views  # Import the views module
 
 urlpatterns = [
-    path('', views.index, name="index"),  # Pass the view function directly
+    path('', views.index, name="index"),
+    path('all_emp',views.all_emp,name="all emp")  # Pass the view function directly
 ]
--- emp_app/views.py
+++ emp_app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+from .models import Employe,Role,dept
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def all_emp(request):
+    emps=Employe.objects.all()
+    context={
+        "emps":emps
+    }
+    return render(request,'view_all.html',context)
