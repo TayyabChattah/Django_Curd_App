429018b923a299a066fb3300e5ff6f9d4141bba1
TayyabChattah <tayyyabchattah72@gmail.com>
2025-02-04 22:58:59 +0500

    Add done

--- emp_app/urls.py
+++ emp_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views  # Import the views module
+from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('all_emp',views.all_emp,name="all emp")  # Pass the view function directly
+    path('', views.index, name='index'),
+    path('all_emp/', views.all_emp, name='all_emp'),
+    path('add_emp/', views.add_emp, name='add_emp'),
 ]
--- emp_app/views.py
+++ emp_app/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime 
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Employe,Role,dept
 # Create your views here.
@@ -10,3 +12,34 @@ def all_emp(request):
         "emps":emps
     }
     return render(request,'view_all.html',context)
+def add_emp(request):
+    if request.method == "POST":
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        salary = int(request.POST['salary'])
+        bonus = int(request.POST['bonus'])
+        phone = int(request.POST['phone'])
+        dept_id = int(request.POST['dept'])
+        role_id = int(request.POST['role'])
+        new_emp = Employe(
+            first_name=first_name,
+            last_name=last_name,
+            salary=salary,
+            bonous=bonus,
+            Phone=phone,
+            dept_id=dept_id,
+            Role_id=role_id,
+            hire_date=datetime.now()
+        )
+        new_emp.save()
+        return HttpResponse("Employee added Successfully")
+    elif request.method == 'GET':
+        departments = dept.objects.all()  # Fetch all departments
+        roles = Role.objects.all()  # Fetch all roles
+        context = {
+            'departments': departments,
+            'roles': roles
+        }
+        return render(request, 'add_emp.html', context)
+    else:
+        return HttpResponse("An error occurred")
